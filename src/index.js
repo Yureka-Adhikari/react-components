2f7f8d5b1d78b177aaaa3e88f27351bc92cec566
Yureka Adhikari <adhikariyureka@gmail.com>
2024-10-16 17:07:21 +0545

    YAYYYY

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+class Student extends React.Component {
+  render() {
+    return <h2> Hi, my name is Yureka.</h2>;
+  }
+}
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Student />
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
